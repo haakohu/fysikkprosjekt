Fix numerical solution

1.py
@@ -9,28 +9,15 @@ def get_info_from_file(filepath):
   return t,x
 
 
+t1,x1 = get_info_from_file("python/utenfjar2.txt")
+t2,x2 = get_info_from_file("python/utenfjar1.txt")
+x1 /= 100
+x2 /= 100
 
-t,x = get_info_from_file("python/utenfjar2.txt")
-x /= 100
-
-n = len(t)
-u = []
-vinkel = pi/4
-g = 9.81
-half_size = floor(n/4)
-t = t[half_size:n]
-x = x[half_size:n]
-
-for i in range(0,n-half_size):
-  new_value = cos(vinkel) / sin(vinkel) - 2*x[i] / (g * t[i]**2 * sin(vinkel))
-  u.append(new_value)
-
-
-
-
-print(u)
-
-print(np.average(u))
-#plt.plot(t,u)
-#plt.show()
-
+a1 = 2* (x1[-1] -x1[0])/(t1[-1] - t1[0])**2
+a2 = 2* (x2[-1] -x2[0])/(t2[-1] - t2[0])**2
+av = (a1+a2) / 2
+vinkel = pi / 4
+g = 9.82
+my = av/(g*cos(vinkel))
+print(my)

2.py
@@ -14,27 +14,40 @@ t,x = get_info_from_file("python/fjar1.txt")
 
 x /= 100
 
-n = len(x)
+N = len(x)
 v = [0]
 a = [0,0]
 m = 0.256
 g = 9.81
 vinkel = pi / 4
-delta_t = t[-1] / n
-v = [0]
-#g = lambda i: m * g*(cos(vinkel) - u * sin(vinkel)) - 
-for i in range(1,n):
-  new_value = x[i] - x[i-1]
-  new_value /= delta_t
-  v.append(new_value)
-  
-for i in range(1,n-1):
-  new_value = v[i] - v[i-1]
-  a.append(new_value / delta_t)
+delta_t = t[-1] / N
+my = 0.583141176884
+k = 65
+print(N)
+x_numerical = [x[0]]
+v_numerical = [0]
+G_first = lambda x: (m*g*sin(vinkel) + my*m*g*cos(vinkel) - k * x) / m
+G_last  = lambda x: (m*g*sin(vinkel) - my*m*g*cos(vinkel) - k * x) / m
+#Finne når klossen endrer retning
+turning_point = -5
 
+for i in range(1,N):
+  if x[i] > x[i-1]:
+    turning_point = i
+    break
+print(turning_point)
 
+for i in range(0,turning_point):
+  x_numerical.append(x_numerical[-1] + delta_t * v_numerical[-1])
+  v_numerical.append(v_numerical[-1] + delta_t * G_first(x_numerical[-2]))
+
+for i in range(turning_point,N):
+  x_numerical.append(x_numerical[-1] + delta_t * v_numerical[-1])
+  v_numerical.append(v_numerical[-1] + delta_t * G_last(x_numerical[-2]))
 
 # plt.plot(t,v)
-plt.plot(t,v)
+print(x_numerical)
+plt.plot(t,x_numerical[0:-1])
+plt.plot(t,x)
 print(np.average(a))
 plt.show()